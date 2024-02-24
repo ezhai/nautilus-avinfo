718c6b92187865835fc37becabdd06bf92a2b13a
Eric Zhai <ezhai.dev@gmail.com>
2024-02-24 14:39:05 -0800

    fix repo link

--- .github/actions/fedora-build-docker/entrypoint.sh
+++ .github/actions/fedora-build-docker/entrypoint.sh
@@ -5,6 +5,10 @@ repository=${1}
 cd /github/workspace
 git clone "${repository}"
 cd nautilus-avinfo
+if [[ $? -ne 0 ]]; then
+    echo "Failed to checkout repository"
+    exit 1
+fi
 
 meson setup build
 meson compile -C build
