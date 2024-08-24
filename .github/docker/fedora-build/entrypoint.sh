42be367cc8b48f3e01c4d61732624103dd0a9d32
Eric Zhai <ezhai.dev@gmail.com>
2024-08-23 21:36:23 -0700

    try creating custom config

--- .github/docker/fedora-build/entrypoint.sh
+++ .github/docker/fedora-build/entrypoint.sh
@@ -2,6 +2,9 @@
 
 branch=${1}
 
+echo -e "include('centos-stream-10-aarch64.cfg')\ninclude('templates/rpmfusion_free-epel.tpl')" >> /etc/mock/centos-stream-10-rpmfusion-aarch64.cfg
+echo -e "include('centos-stream-10-x86_64.cfg')\ninclude('templates/rpmfusion_free-epel.tpl')" >> /etc/mock/centos-stream-10-rpmfusion-x86_64.cfg
+
 git clone https://github.com/ezhai/nautilus-avinfo.git
 cd nautilus-avinfo
 git checkout "${branch}"
@@ -30,15 +33,13 @@ rpmdev-setuptree
 mv "${pkgname}.tar.gz" ~/rpmbuild/SOURCES/
 cat pkg/rpm/nautilus-avinfo.spec.template | VERSION=${version} envsubst > ~/rpmbuild/SPECS/nautilus-avinfo.spec
 
-sudo yum localinstall --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm
-
 cd ~/rpmbuild
 rpmbuild -bs "SPECS/nautilus-avinfo.spec"
-mock -r centos-stream-10-aarch64 -a "http://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.aarch64/" "$(find ~/rpmbuild/SRPMS/ -regex ".*\.src\.rpm")"
-mock -r centos-stream-10-x86_64 -a "http://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.x86_64/" "$(find ~/rpmbuild/SRPMS/ -regex ".*\.src\.rpm")"
+mock -r centos-stream-10-rpmfusion-aarch64 "$(find ~/rpmbuild/SRPMS/ -regex ".*\.src\.rpm")"
+mock -r centos-stream-10-rpmfusion-x86_64 "$(find ~/rpmbuild/SRPMS/ -regex ".*\.src\.rpm")"
 
 mkdir -p /github/rpm/
 cp $(find ~/rpmbuild/SPECS/ -regex ".*\.spec") /github/rpm
 cp $(find ~/rpmbuild/SRPMS/ -regex ".*\.src\.rpm") /github/rpm
-cp $(find /var/lib/mock/centos-stream-10-aarch64/result/ -regex ".*\.rpm") /github/rpm
-cp $(find /var/lib/mock/centos-stream-10-x86_64/result/ -regex ".*\.rpm") /github/rpm
+cp $(find /var/lib/mock/centos-stream-10-rpmfusion-aarch64/result/ -regex ".*\.rpm") /github/rpm
+cp $(find /var/lib/mock/centos-stream-10-rpmfusion-x86_64/result/ -regex ".*\.rpm") /github/rpm
