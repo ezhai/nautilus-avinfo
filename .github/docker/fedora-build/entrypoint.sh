8f7364893d1b2a3574cdb933352f9dad04cfcea3
Eric Zhai <ezhai.dev@gmail.com>
2024-08-25 21:40:41 -0700

    try epel

--- .github/docker/fedora-build/entrypoint.sh
+++ .github/docker/fedora-build/entrypoint.sh
@@ -21,27 +21,36 @@ fi
 version=$(meson introspect build --projectinfo | jq -r ".version")
 pkgname="nautilus-avinfo-${version}"
 
-# Set up rpmbuild
+# Set up source
 git clone https://github.com/ezhai/nautilus-avinfo.git "${pkgname}/"
 cd "${pkgname}/"
 git checkout "${branch}"
 cd ..
 tar -cvzf "${pkgname}.tar.gz" "${pkgname}/"
 
+# Set up RPM build directory
 rpmdev-setuptree
 mv "${pkgname}.tar.gz" ~/rpmbuild/SOURCES/
 cat pkg/rpm/nautilus-avinfo.spec.template | VERSION=${version} envsubst > ~/rpmbuild/SPECS/nautilus-avinfo.spec
 
-# Build RPM
+# Build SRPM
 cd ~/rpmbuild
 rpmbuild -bs "SPECS/nautilus-avinfo.spec"
 
-mock -r centos-stream-9-aarch64 --install "https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm"
-mock -r centos-stream-9-aarch64 --install "https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-9.noarch.rpm"
-mock -r centos-stream-9-aarch64 --no-clean "$(find ~/rpmbuild/SRPMS/ -regex ".*\.src\.rpm")"
+# Centos 9
+# mock -r centos-stream-9-aarch64 --install "https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm"
+# mock -r centos-stream-9-aarch64 --install "https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-9.noarch.rpm"
+# mock -r centos-stream-9-aarch64 --no-clean "$(find ~/rpmbuild/SRPMS/ -regex ".*\.src\.rpm")"
 
+# EPEL 9
+mock -r epel-9-aarch64 --install "https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm"
+mock -r epel-9-aarch64 --install "https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-9.noarch.rpm"
+mock -r epel-9-aarch64 --no-clean "$(find ~/rpmbuild/SRPMS/ -regex ".*\.src\.rpm")"
+
+# Fedora 40
 mock -r fedora-40-aarch64 --no-clean "$(find ~/rpmbuild/SRPMS/ -regex ".*\.src\.rpm")"
 
+# Upload artifacts
 mkdir -p /github/rpm/
 cp $(find ~/rpmbuild/SPECS/ -regex ".*\.spec") /github/rpm
 cp $(find ~/rpmbuild/SRPMS/ -regex ".*\.src\.rpm") /github/rpm
