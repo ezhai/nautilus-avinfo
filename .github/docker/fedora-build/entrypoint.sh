05f63be5dd3d045f7638974cd91faf651bf918db
ezhai <43300788+ezhai@users.noreply.github.com>
2024-08-25 23:49:33 -0700

    Fix broken RPM dependencies (#3)
    * fix broken nautilus dependency
    
    * use conditional dependency for ffmpeg

--- .github/docker/fedora-build/entrypoint.sh
+++ .github/docker/fedora-build/entrypoint.sh
@@ -2,6 +2,7 @@
 
 branch=${1}
 
+# Get build info
 git clone https://github.com/ezhai/nautilus-avinfo.git
 cd nautilus-avinfo
 git checkout "${branch}"
@@ -20,22 +21,32 @@ fi
 version=$(meson introspect build --projectinfo | jq -r ".version")
 pkgname="nautilus-avinfo-${version}"
 
+# Set up source
 git clone https://github.com/ezhai/nautilus-avinfo.git "${pkgname}/"
 cd "${pkgname}/"
 git checkout "${branch}"
 cd ..
 tar -cvzf "${pkgname}.tar.gz" "${pkgname}/"
 
+# Set up RPM build directory
 rpmdev-setuptree
 mv "${pkgname}.tar.gz" ~/rpmbuild/SOURCES/
 cat pkg/rpm/nautilus-avinfo.spec.template | VERSION=${version} envsubst > ~/rpmbuild/SPECS/nautilus-avinfo.spec
 
+# Build SRPM
 cd ~/rpmbuild
 rpmbuild -bs "SPECS/nautilus-avinfo.spec"
-mock -r fedora-40-aarch64 "$(find ~/rpmbuild/SRPMS/ -regex ".*\.src\.rpm")"
-mock -r fedora-40-x86_64 "$(find ~/rpmbuild/SRPMS/ -regex ".*\.src\.rpm")"
 
+# EPEL 9
+# mock -r centos-stream+epel-9-aarch64 --install "https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm"
+# mock -r centos-stream+epel-9-aarch64 --install "https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-9.noarch.rpm"
+# mock -r centos-stream+epel-9-aarch64 --no-clean "$(find ~/rpmbuild/SRPMS/ -regex ".*\.src\.rpm")"
+
+# Fedora 40
+mock -r fedora-40-aarch64 --no-clean "$(find ~/rpmbuild/SRPMS/ -regex ".*\.src\.rpm")"
+
+# Upload artifacts
 mkdir -p /github/rpm/
+cp $(find ~/rpmbuild/SPECS/ -regex ".*\.spec") /github/rpm
 cp $(find ~/rpmbuild/SRPMS/ -regex ".*\.src\.rpm") /github/rpm
-cp $(find /var/lib/mock/fedora-40-aarch64/result/ -regex ".*\.rpm") /github/rpm
-cp $(find /var/lib/mock/fedora-40-x86_64/result/ -regex ".*\.rpm") /github/rpm
+cp $(find /var/lib/mock/*/result/ -regex ".*\.rpm") /github/rpm
