5ea1de528e54d0b3c4a847158aedda1d4fc385a0
Eric Zhai <ezhai.dev@gmail.com>
2024-08-31 10:54:08 -0700

    use optional ffmpeg-devel dep

--- scripts/generate-rpm-spec.sh
+++ scripts/generate-rpm-spec.sh
@@ -3,8 +3,8 @@
 version="$1"
 specdir="$2"
 
-ffmpeg_dev_fedora="ffmpeg-free-devel"
+ffmpeg_dev_fedora="(ffmpeg-devel or ffmpeg-free-devel)"
 ffmpeg_dev_opensuse="ffmpeg-5-libavcodec-devel ffmpeg-5-libavformat-devel ffmpeg-5-libavutil-devel"
 
-cat "${specdir}/nautilus-avinfo.spec.template" | VERSION=${version} FFMPEG_DEV=${ffmpeg_dev_fedora} envsubst > "${specdir}/nautilus-avinfo.fc.spec"
+cat "${specdir}/nautilus-avinfo.spec.template" | VERSION=${version} FFMPEG_DEV=${ffmpeg_dev_fedora} envsubst > "${specdir}/nautilus-avinfo.fedora.spec"
 cat "${specdir}/nautilus-avinfo.spec.template" | VERSION=${version} FFMPEG_DEV=${ffmpeg_dev_opensuse} envsubst > "${specdir}/nautilus-avinfo.opensuse.spec"
