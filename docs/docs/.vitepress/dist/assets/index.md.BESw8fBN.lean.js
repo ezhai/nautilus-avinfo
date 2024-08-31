3a6c1e0698f14e9d95fb25a2a94b0735b617b484
Eric Zhai <ezhai.dev@gmail.com>
2024-08-30 19:21:53 -0700

    remove dist

--- docs/docs/.vitepress/dist/assets/index.md.BESw8fBN.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as a,o as t,a2 as s}from"./chunks/framework.C94oF1kp.js";const m=JSON.parse('{"title":"User Guide","description":"","frontmatter":{},"headers":[],"relativePath":"index.md","filePath":"index.md"}'),i={name:"index.md"},o=s("",24),n=[o];function l(d,r,p,c,h,u){return t(),a("div",null,n)}const g=e(i,[["render",l]]);export{m as __pageData,g as default};
