8f18bb24684caf606a3fad6a58180295117b6212
Eric Zhai <ezhai.dev@gmail.com>
2025-11-11 17:18:52 -0800

    fix missing libgnome-desktop package for fedora

--- .github/docker/arch-build/entrypoint.sh
+++ .github/docker/arch-build/entrypoint.sh
@@ -4,6 +4,15 @@ branch="${1}"
 builddir="$(pwd)"
 retcode=0
 
+# Checkout project
+git clone https://github.com/ezhai/nautilus-avinfo.git
+cd nautilus-avinfo
+git checkout "${branch}"
+if [[ $? -ne 0 ]]; then
+    echo "Branch \"${branch}\" does not exist"
+    exit 1
+fi
+
 # Build project
 cd pkg/aur
 makepkg -si --noconfirm
